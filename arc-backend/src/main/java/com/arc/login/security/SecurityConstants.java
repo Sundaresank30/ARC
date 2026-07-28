d00aa4ef2d106ac119df2ab033ebe16e417ed2e3
sundar <sundarkanniyappan30@gmail.com>
2026-07-28 20:43:18 +0530

    Remove arc-backend directory

--- arc-backend/src/main/java/com/arc/login/security/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.arc.login.security;
-
-public class SecurityConstants {
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/auth/register";
-    public static final String SIGN_IN_URL = "/api/auth/login";
-}
